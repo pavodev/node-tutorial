9ffb25fee3dcee7647023a0006b860fdbc457481
pavodev <pavici@usi.ch>
2020-08-24 17:00:18 +0200

    refactoring

---
+++ 4-natours/controllers/tourController.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkId = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+
+  if (val > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  next();
+};
+
+// HANDLERS
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    requestedAt: req.requestTime,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1; // trick
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    // results: tours.length,
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      }); // 201 CREATED
+    }
+  );
+
+  // res.send('Done');
+};
+
+exports.updateTour = (req, res) => {
+  const id = req.params.id;
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+  // will be implemented in the MongoDB section
+};
+
+exports.deleteTour = (req, res) => {
+  const id = req.params.id;
+
+  res.status(204).json({
+    // no content code
+    status: 'success',
+    data: null,
+  });
+  // will be implemented in the MongoDB section
+};
---
+++ 4-natours/controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    stats: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    stats: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    stats: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    stats: 'error',
+    message: 'This route is not yet deusfined',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    stats: 'error',
+    message: 'This route is not yet defined',
+  });
+};
