cad40ed8bec45fba75dfe5453ce2b142f96957bc
pavodev <pavici@usi.ch>
2020-08-25 18:26:54 +0200

    added environment variales

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config({
+  path: './config.env',
+});
+
 const app = require('./app');
 
-const port = 3000;
+// by default it is set by express to 'development'
+// console.log(app.get('env'));
+
+// Node environement variables
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
