dfbb643bf893759e41b48e22dd356da8726455a8
Ivan Pavic <ivan.pavic@usi.ch>
2021-08-04 16:10:49 +0200

    routes refactoring, authentication and authorization refactoring

--- 4-natours/controllers/reviewController.js
+++ 4-natours/controllers/reviewController.js
@@ -1,54 +1,20 @@
 const Review = require('../models/reviewModel');
-const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
+// const AppError = require('../utils/appError');
+// const catchAsync = require('../utils/catchAsync');
+
+const factory = require('./handlerFactory');
 
 // HANDLERS
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  // EXECUTE THE QUERY
-  const reviews = await Review.find(filter);
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
-
-exports.getReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findById(req.params.id);
-
-  if (!review) {
-    next(new AppError('No review found with that ID', 404));
-    return;
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      review,
-    },
-  });
-});
-
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    },
-  }); // 201 CREATED
-});
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
