5f43114b2e72f81e9d04795a0d402745a4306ceb
Ivan Pavic <ivan.pavic@usi.ch>
2021-08-04 10:22:15 +0200

    implemented nested routes

--- 4-natours/routes/reviewRoutes.js
+++ 4-natours/routes/reviewRoutes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router();
+/*
+ * mergeParams: by default each router has access to the params of its specific routes.
+ * As we are using nested routes, we must tell express to merge parameter from the previous router.
+ */
+const router = express.Router({ mergeParams: true });
+
+// POST /reviews
+// GET /tour/234/reviews
+// POST /tour/234/reviews
 
 router
   .route('/')
