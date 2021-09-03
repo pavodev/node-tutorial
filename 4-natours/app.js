32f38b8b68509a01f152a98126ad3688b1977195
Ivan Pavic <ivan.pavic@usi.ch>
2021-09-03 14:36:17 +0200

    implemented overview and tour templates

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express(); // adds methods to app
 
@@ -80,10 +81,7 @@ app.use((req, res, next) => {
 
 /*************** Routers **************/
 
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
