c6bd4947ff9859cde6c3a0207d00e3aa86cdaae0
Ivan Pavic <ivan.pavic@usi.ch>
2021-09-03 14:36:17 +0200

    implemented overview and tour templates

---
+++ 4-natours/controllers/viewsController.js
@@ -0,0 +1,27 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Buildthe template
+
+  // 3) Render that template using the tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour,
+  });
+});
