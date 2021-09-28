16e889fea5c4e2069639582fb1b828cec8f82d3d
Ivan Pavic <ivan.pavic@usi.ch>
2021-09-28 14:11:04 +0200

    fixed login form: content security policies were not defined

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,20 +1,23 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+const csp =
+  "default-src 'self' https://js.stripe.com/v3/ https://cdnjs.cloudflare.com https://api.mapbox.com; base-uri 'self'; block-all-mixed-content; connect-src 'self' https://js.stripe.com/v3/ https://cdnjs.cloudflare.com/ https://*.mapbox.com/; font-src 'self' https://fonts.google.com/ https: data:;frame-ancestors 'self'; img-src 'self' data:; object-src 'none'; script-src 'self' https://js.stripe.com/v3/ https://cdnjs.cloudflare.com/ https://api.mapbox.com/ blob:; script-src-attr 'none'; style-src 'self' https: 'unsafe-inline'; upgrade-insecure-requests;";
+
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
   // 2) Buildthe template
 
   // 3) Render that template using the tour data from 1)
-  res.status(200).render('overview', {
+  res.status(200).set('Content-Security-Policy', csp).render('overview', {
     title: 'All Tours',
     tours,
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
@@ -22,12 +25,15 @@ exports.getTour = catchAsync(async (req, res) => {
 
   res
     .status(200)
-    .set(
-      'Content-Security-Policy',
-      'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com'
-    )
+    .set('Content-Security-Policy', csp)
     .render('tour', {
       title: `${tour.name} Tour`,
       tour,
     });
 });
+
+exports.getLoginFormResponse = (req, res) => {
+  res.status(200).set('Content-Security-Policy', csp).render('login', {
+    title: 'Log into your account',
+  });
+};
