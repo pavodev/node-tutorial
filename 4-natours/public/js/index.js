4c2494ddd238aed9d895317b3f5b98ed8e780906
Ivan Pavic <ivan.pavic@usi.ch>
2021-09-28 23:08:49 +0200

    implemented user change password

---
+++ 4-natours/public/js/index.js
@@ -0,0 +1,68 @@
+/* eslint-disable */
+
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
+
+// DOM ELEMENTS
+
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form--login');
+const logoutButton = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
+
+//DELEGATION
+
+if (mapBox) {
+  const locations = JSON.parse(map.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevents the form to load any page
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
+
+if (logoutButton) {
+  logoutButton.addEventListener('click', logout);
+}
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+}
