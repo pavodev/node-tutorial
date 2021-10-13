c07ca251f52fcf2eb4c00716bc50cb034e406d52
Ivan Pavic <ivan.pavic@usi.ch>
2021-10-13 17:06:25 +0200

    implemented complex mail handler, implemented image upload with multer

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,17 +1,56 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-
 const factory = require('./handlerFactory');
 
-const filterObject = (obj, ...allowedFields) => {
-  const newObject = {};
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
+const multerStorage = multer.memoryStorage();
 
-  Object.keys(obj).forEach((key) => {
-    if (allowedFields.includes(key)) newObject[key] = obj[key];
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
   });
-
-  return newObject;
+  return newObj;
 };
 
 exports.getMe = (req, res, next) => {
@@ -19,24 +58,24 @@ exports.getMe = (req, res, next) => {
   next();
 };
 
-// for currently logged in user
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
-
-  if (req.body.password || req.body.passwordConfirm)
+  if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
-        'This route is not for password updates. Please use /updateMyPassword',
+        'This route is not for password updates. Please use /updateMyPassword.',
         400
       )
     );
+  }
 
-  // 2) Filter out unwanted fields that have been sent in the request
-  const filteredBody = filterObject(req.body, 'name', 'email');
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-    new: true, // returns the updated object instead of the old one
+    new: true,
     runValidators: true,
   });
 
@@ -64,9 +103,9 @@ exports.createUser = (req, res) => {
   });
 };
 
-// Admin
-
-exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
-exports.updateUser = factory.updateOne(User); // Do not change password with this
+exports.getAllUsers = factory.getAll(User);
+
+// Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
